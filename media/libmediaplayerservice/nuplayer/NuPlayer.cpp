d062aaa856c15af860af7f4d961d8030320de8a1
Manish Dewangan <manish@codeaurora.org>
2016-04-07 18:48:39 +0530

    frameworks/av: enable audio extended features
    Enable below extended audio features:
    1. Support Playback and record for extended formats
    2. Support for 24 bit playback and record
    
    frameworks/av: Changes to match exact format for input profile
    Change-Id: Ic1f839600906b2fb84867b4bcb3ab1d159aa0a67
    
    audio: add support for extended audio encoding
    Change-Id: I2f93a88ed5c9bd502399ef145a5802d1a15fcc45
    
    audio: add support for FLAC and AMRWB+ decoding
    Change-Id: Ia35b0b9218fdf2584348a2a95bf0401c5065dedc
    
    audio: make sure qti sw codec are allocated successfully
    Change-Id: Ic95ef93da95b32c24aadbe626d19fea23d2ca7a6
    
    audio: add support for playback of DSD clips.
    Change-Id: I1fd5af88a830b4c8577f6be59f07ebdac7476a9b
    
    audio: add encoding support for linear PCM format
    Change-Id: Iecf402064d67d35d5057b814c842d79af92756d0
    
    audio: calculate the max buffer size runtime
    Change-Id: Ie7d43ddfdcb2b13a8a5eaa12cc8a233ac0d9470d
    
    audio: Update with correct audio format for WMA Pro clips.
    Change-Id: I75c7ed56a437c818d7072dbd5092db24bce1baad
    
    audio: update extended audioSource as per AOSP code
    Change-Id: I2dabb6d34daa30df47501518a417e13a2543cc35
    
    MediaCodec: reset size to zero for EOS.
    Change-Id: I31727a00e0edf5604d0409fecbd976334508e6d7
    
    audio: Dataconverter changes to support pcm 24bit
    Change-Id: Ic3745bccea43f1be9483c251beea0dd45fa443db
    
    audio: Dataconverter changes to support pcm 24bit
    Change-Id: I6479e9fb34c27cda912106a9165bf3a6631c2d80
    
    audio: Remove unused code related to NuPlayer pcm conversion
    Change-Id: I198d0be5fe02ca2dbde2502713edf99b92ca1399
    
    nuplayer: set maxBuffers to 1 in byte stream mode
    Change-Id: I4f51bbc4c53d17f62cab571b83ca015cea430fa4
    
    libmedia: correct latency computing of AudioRecord
    Change-Id: I26e5e47e8cc3720895b962f7aab8a595a54b7c83
    
    StagefrightRecorder: add null check for recording source
    Change-Id: I8dda57f39bbf5e289facc5ce261d53d988630620
    
    libmedia : Fix for AudioRecord CTS test case
    Change-Id: Icb1e717d869de7bcd4b52194f3ee4e8b0b318da4
    
    frameworks/av: enable bytestream mode in extractor selectively
    Change-Id: I74c2236fa70ae60da8fbcb1fbabb5a66b43b272c
    
    Change-Id: I5d0af2bd24e598bdd34fe201d7898cad94887c6f
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -1809,6 +1809,7 @@ status_t NuPlayer::instantiateDecoder(
             *decoder = AVNuFactory::get()->createPassThruDecoder(notify, mSource, mRenderer);
             ALOGV("instantiateDecoder audio DecoderPassThrough hasVideo: %d", hasVideo);
         } else {
+            AVNuUtils::get()->setCodecOutputFormat(format);
             mSource->setOffloadAudio(false /* offload */);
 
             *decoder = AVNuFactory::get()->createDecoder(notify, mSource, mPID, mUID, mRenderer);
