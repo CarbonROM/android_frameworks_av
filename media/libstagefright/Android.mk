06eb53e04901ab8ae8ac4784a0925473da739166
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2016-11-10 01:22:51 +0200

    libstagefright: fix camera/encoder for targets that using native handle source
    Some devices (e.g. Xiaomi Mi4c) using native handle source, activated by
    flag TARGET_USES_MEDIA_EXTENSIONS from hardware/qcom/media-caf.
    
    Change-Id: Ifbf477218bca9b56a8f1cce2601ebc4f6a37fee4

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -141,8 +141,18 @@ LOCAL_SHARED_LIBRARIES += \
         libdl \
         libRScpp \
 
+ifeq ($(BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21), true)
+# This needs flag requires the following string constant in
+# CameraParametersExtra.h:
+#
+# const char CameraParameters::PIXEL_FORMAT_YUV420SP_NV21[] = "nv21";
+LOCAL_CFLAGS += -DUSE_SAMSUNG_CAMERAFORMAT_NV21
+endif
+
+ifneq ($(TARGET_USES_MEDIA_EXTENSIONS),true)
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1),true)
 LOCAL_CFLAGS += -DCAMCORDER_GRALLOC_SOURCE
+endif
 endif
 
 LOCAL_CFLAGS += -Wno-multichar -Werror -Wno-error=deprecated-declarations -Wall
