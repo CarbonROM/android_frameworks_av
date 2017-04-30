29013eb3fe7d09122a048a862671d39949edaaaf
Simao Gomes Viana <xdevs23@outlook.com>
2017-04-30 15:19:54 +0200

    libcameraservice: do not rotate on flag
    Logic was reversed
    
    Change-Id: If47c42605ff3108cea18a02b152e348130dce0c2

--- services/camera/libcameraservice/api2/CameraDeviceClient.cpp
+++ services/camera/libcameraservice/api2/CameraDeviceClient.cpp
@@ -554,6 +554,7 @@ binder::Status CameraDeviceClient::createStream(
                 " (%d x %d) with format 0x%x.",
               __FUNCTION__, mCameraId, streamId, width, height, format);
 
+#ifndef CAMERA_STREAM_NO_ROTATION
         // Set transform flags to ensure preview to be rotated correctly.
         res = setStreamTransformLocked(streamId);
 
