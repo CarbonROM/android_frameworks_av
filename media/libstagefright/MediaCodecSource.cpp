a71aa664625f1062090e1a6b8a8c68dacbc6ba32
Paras Nagda <pnagda@codeaurora.org>
2017-05-29 13:44:55 +0530

    libstagefright: call release encoder instead of signalEOS
    Below changes are squashed with this change
    
    libstagefright: Call encoder release first
    ChangeId: I1ce673595be1b4024ceca4975bebfdbae850fb3e
    
    libstagefright: call release encoder instead of signalEOS
    ChangeId: Id75055e85fad3f6802f9939c7536fa8ce00dcb43
    
    Change-Id: I07cd35aeeab36ed3b4479b086fe4c3b83a41d5c9

--- media/libstagefright/MediaCodecSource.cpp
+++ media/libstagefright/MediaCodecSource.cpp
@@ -197,9 +197,6 @@ void MediaCodecSource::Puller::stop() {
         queue->flush(); // flush any unprocessed pulled buffers
     }
 
-    if (interrupt) {
-        interruptSource();
-    }
 }
 
 void MediaCodecSource::Puller::interruptSource() {
@@ -1068,6 +1065,8 @@ void MediaCodecSource::onMessageReceived(const sp<AMessage> &msg) {
         if (generation != mGeneration) {
              break;
         }
+
+        releaseEncoder();
 
         if (!(mFlags & FLAG_USE_SURFACE_INPUT)) {
             ALOGV("source (%s) stopping", mIsVideo ? "video" : "audio");
