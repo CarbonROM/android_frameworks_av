b991bfd7d6580244315b049e37a603ab1d1407b0
Anubhav Phukan <anubhavp@codeaurora.org>
2017-06-16 16:12:21 +0530

    media: Set generation in observer notification message
    A new observer is created on reallocation of component.
    The generation parameter is not set when we do so and
    hence CHECK fails when we check the OMX Message.
    
    Change-Id: I3b8d54c3d0958daa5726df9d76c14babe390e045

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -7774,6 +7774,7 @@ void ACodec::forceStateTransition(int generation) {
 sp<IOMXObserver> ACodec::createObserver() {
     sp<CodecObserver> observer = new CodecObserver;
     sp<AMessage> notify = new AMessage(kWhatOMXMessageList, this);
+    notify->setInt32("generation", this->mNodeGeneration);
     observer->setNotificationMessage(notify);
     return observer;
 }
