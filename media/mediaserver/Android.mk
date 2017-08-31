7cb07608e411c584d6c20403538bd70e9e590b75
Simon Shields <keepcalm444@gmail.com>
2017-08-31 18:24:56 +0200

    Fix cherry-pick: "camera/media: Support for legacy camera HALv1"
    * The commit we had was missing this code.
    
    Change-Id: If4df6f674e9c1e5893883eee9e37a545a1d8e01c

--- media/mediaserver/Android.mk
+++ media/mediaserver/Android.mk
@@ -42,7 +42,12 @@ ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1),true)
     LOCAL_CFLAGS += -DNO_CAMERA_SERVER
 
     LOCAL_SHARED_LIBRARIES += \
-        libcameraservice
+        libcameraservice \
+        android.hardware.camera.common@1.0 \
+        android.hardware.camera.provider@2.4
+
+     LOCAL_STATIC_LIBRARIES += \
+        libarect
 endif
 
 include $(BUILD_EXECUTABLE)
